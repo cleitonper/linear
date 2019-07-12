d485a551723e75956b579d60a2c7b1a44cbfc29e
Cleiton Pereira <cleiton.spereira@live.com>
2019-07-12 14:52:13 -0300

    🚸 auto focus first element of forms

--- src/helpers/generateMatrixFields.tsx
+++ src/helpers/generateMatrixFields.tsx
@@ -4,14 +4,16 @@ import { Input } from 'components/Form';
 export const generateMatrixFields = (
   rows: number,
   columns: number,
-  name = ''
+  name = '',
+  autoFocus = false,
 ): ReactElement[] => {
   const fields = [];
   for (let row = 0; row < rows; ++row) {
     for (let column = 0; column < columns; ++column) {
+      const hasAutoFocus = (row === 0 && column === 0) && autoFocus;
       const placeholder = `${name}${row + 1}${column + 1}`;
       fields.push(
-        <Input key={placeholder} name={placeholder} placeholder={placeholder} />
+        <Input key={placeholder} name={placeholder} placeholder={placeholder} autoFocus={hasAutoFocus} />
       );
     }
   }
