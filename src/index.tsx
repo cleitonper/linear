6ae7e85ae434537cc2d0a70441d2b878c806e8f6
Cleiton Pereira <cleiton.spereira@live.com>
2019-07-12 18:26:17 -0300

    ✨ add service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Yup from 'yup';
 import { PTBR } from 'locales/yup/pt-BR';
+import * as serviceWorker from './serviceWorker';
 
 import 'assets/css/global.css';
 
 import { App } from 'components/App';
+
+serviceWorker.register();
 
 Yup.setLocale(PTBR);
 
